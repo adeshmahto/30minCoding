05bd1baafdb41a34175e8a49de7e448911391faf
Anish Gupta <anishgupta675@gmail.com>
2023-04-30 13:36:17 +0530

    added mail service

--- src/main/java/com/coding/web/exception/ApiException.java
+++ src/main/java/com/coding/web/exception/ApiException.java
@@ -1,9 +1,13 @@
 package com.coding.web.exception;
 
 public class ApiException extends RuntimeException{
+
+    public ApiException(String message, Exception exception) {
+        super(message, exception);
+    }
+
     public ApiException(String message) {
         super(message);
-
     }
 
     public ApiException() {
